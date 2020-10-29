7a070e5bede7851419baf8dc7f651473a21a1b9f
Francesco Bianco <info.francescobianco@gmail.com>
2020-10-29 17:32:11 +0100

    fix

--- test.sh
+++ test.sh
@@ -2,10 +2,13 @@
 
 docker-compose up -d
 
+## Create a test file to upload
 dd if=/dev/zero of=large_file.zip bs=1G count=1
 
-curl -H "Auth..." -F @large_file.zip  http://localhost:8080
+## Run upload using curl
+curl -H "Auth..." -F @large_file.zip http://localhost:8080
 
+## Test if file exists
 if [[ -f "files/large_file.zip" ]]; then
   echo "Test passed."
 else
